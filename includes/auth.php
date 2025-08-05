be1b2ed49d87f5e263b5497edc4f9d3fdb40c711
MrChikip <mrchikip@users.noreply.github.com>
2025-08-05 13:23:37 -0500

    test push

--- includes/auth.php
+++ includes/auth.php
@@ -1,6 +1,7 @@
 <!-- Firebase Authentication Configuration -->
 <!-- Usar la versión más reciente de Firebase SDK -->
 <script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"></script>
+
 <script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-auth-compat.js"></script>
 
 <script>
